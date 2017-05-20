db13d0c84422f3076c827b9534b6b9ee207cd62a
YiSheng <upsetboyincludeme830820@gmail.com>
2017-05-21 02:14:29 +0800

    test

--- HW3_102062335_Cli.c
+++ HW3_102062335_Cli.c
@@ -7,13 +7,31 @@ typedef struct {
 	char name[30];
 	char ip[100];
 	int port;
+	char files[200][40];
+	int filecount;
 } Member;
 Member self;
+
+typedef struct {
+	int connfd;
+	int all;
+	int part;
+	char name[40];
+}sendinfo;
+
+FILE *file;
+pthread_mutex_t fileLock = PTHREAD_MUTEX_INITIALIZER;
 
 void help();
 void updatef(int sockfd);
 void *tcpListener();
+void *doSomething(void *arg);
+void *recf( void * arg);
 char user[40];
+int filefinished[200];
+
+char serIP[40];
+int serPort;
 
 int main(int argc, char **argv) {
 	int sockfd;
@@ -28,6 +46,9 @@ int main(int argc, char **argv) {
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
 	connect(sockfd, (SA*)&servaddr, sizeof(servaddr));
+
+	sprintf(serIP, "%s", argv[1]);
+	serPort = atoi(argv[2]);
 
 	char msg[MAXLINE];
 
@@ -82,9 +103,6 @@ int main(int argc, char **argv) {
 			printf("%s", msg);
 			bzero(msg, sizeof(msg));
 		}
-		if(!strcmp(msg, "updatef")) {
-			updatef(sockfd);
-		}
 		if(!strcmp(msg, "listf")) {
 			//send cmd to server
 			write(sockfd, msg, strlen(msg));
@@ -92,6 +110,17 @@ int main(int argc, char **argv) {
 			read(sockfd, msg, MAXLINE);
 			printf("%s", msg);
 			bzero(msg, sizeof(msg));
+		}
+		if(!strcmp(msg, "infos")) {
+			//send cmd to server
+			write(sockfd, msg, strlen(msg));
+			bzero(msg, sizeof(msg));
+			read(sockfd, msg, MAXLINE);
+			printf("%s", msg);
+			bzero(msg, sizeof(msg));
+		}
+		if(!strcmp(msg, "updatef")) {
+			updatef(sockfd);
 		}
 		if(!strcmp(msg, "connect")) {
 			char ip[40];
@@ -110,8 +139,224 @@ int main(int argc, char **argv) {
 			puts(msg);
 			bzero(msg, sizeof(msg));
 		}
+		if(!strcmp(msg, "get")) {
+			char filename[40];
+			bzero(msg, sizeof(msg));
+			// get file list
+			write(sockfd, "infos", strlen("infos"));
+			read(sockfd, msg, MAXLINE);
+			//write into a real file
+			FILE *flist;
+			flist = fopen("list", "w");
+			if(flist != NULL) {
+				fprintf(flist, "%s", msg);
+				fclose(flist);
+			}
+			//read back into mem struct
+			Member member[100];
+			flist = fopen("list", "r");
+			int idx = 0;
+			if(flist != NULL) {
+				while(~fscanf(flist, "%s%d", member[idx].name, &member[idx].filecount)) {
+					fscanf(flist, "%s%d", member[idx].ip, &member[idx].port);
+					if(!strcmp(member[idx].name, "server")) {
+						sprintf(member[idx].ip, "%s", serIP);
+						member[idx].port = serPort;
+					}
+					int i;
+					for(i = 0; i < member[idx].filecount; i++)
+						fscanf(flist, "%s", member[idx].files[i]);
+					if(member[idx].filecount)
+						idx++;
+				}
+				fclose(flist);
+			}
+			
+			//print out all infos to check correct or not
+			int j, k;
+			for(j = 0; j < idx; j++) {
+				printf("%s %d\n", member[j].name, member[j].filecount);
+				printf("%s %d\n", member[j].ip, member[j].port);
+				for(k = 0; k < member[j].filecount; k++)
+					printf("%s\n", member[j].files[k]);
+			}
+
+			//which user want...
+			printf("you want>> ");
+			scanf("%s", filename);
+
+			Member tmp[100];
+			int count = 0;
+			for(j = 0; j < idx; j++) {
+				for(k = 0; k < member[j].filecount; k++) {
+					if(!strcmp(filename, member[j].files[k])) {
+						printf("%s %d\n", member[j].name, member[j].filecount);
+						printf("%s %d\n", member[j].ip, member[j].port);
+						printf("%s\n", member[j].files[k]);
+						sprintf(tmp[count].name, "%s", member[j].name);
+						sprintf(tmp[count].ip, "%s", member[j].ip);
+						tmp[count].port = member[j].port;
+						count++;
+					}
+				}
+			}
+			if(count == 0) {
+				printf("no this file\n");
+				continue;
+			}
+			pthread_t aa;
+
+			struct sockaddr_in dstaddr;
+			int dstfd;
+			dstfd = socket(AF_INET, SOCK_STREAM, 0);
+			bzero(&dstaddr, sizeof(dstaddr));
+			if(!strcmp(tmp[0].name, "server"))
+				dstaddr.sin_port = htons(tmp[0].port);
+			else
+				dstaddr.sin_port = htons(1500);
+			dstaddr.sin_family = AF_INET;
+			dstaddr.sin_addr.s_addr = inet_addr(tmp[0].ip);
+			connect(dstfd, (SA*)&dstaddr, sizeof(dstaddr));
+			write(dstfd, "gets", strlen("gets"));
+			write(dstfd, filename, strlen(filename));
+			bzero(msg, sizeof(msg));
+			read(dstfd, msg, MAXLINE);
+			int size = atoi(msg);
+			char tttt[size];
+			memset(tttt, '0', sizeof(tttt));
+			FILE *fp;
+			char dd[80];
+			sprintf(dd, "%s/%s", user, filename);
+			fp = fopen(dd, "w");
+			fprintf(fp, "%s", tttt);
+			fclose(fp);
+
+			for(j = 0; j < count; j++) {
+				struct sockaddr_in dstaddr;
+				int dstfd;
+
+				dstfd = socket(AF_INET, SOCK_STREAM, 0);
+				bzero(&dstaddr, sizeof(dstaddr));
+
+				if(!strcmp(tmp[j].name, "server"))
+					dstaddr.sin_port = htons(tmp[j].port);
+				else
+					dstaddr.sin_port = htons(1500);
+
+				dstaddr.sin_family = AF_INET;
+				dstaddr.sin_addr.s_addr = inet_addr(tmp[j].ip);
+				connect(dstfd, (SA*)&dstaddr, sizeof(dstaddr));
+				write(dstfd, "getf", strlen("getf"));
+
+				sendinfo *d;
+		        d = malloc(sizeof(sendinfo));
+		        d->connfd = dstfd;
+		        d->all = count;
+		        d->part = j;
+		        sprintf(d->name, "%s", filename);
+				pthread_create(&(aa), NULL, &recf, d);
+			}
+			printf("wait....\n");
+			sleep(5);
+			updatef(sockfd);
+			printf("Done....\n");
+		}
+		if(!strcmp(msg, "serf")) {
+			write(sockfd, msg, strlen(msg));
+			bzero(msg, sizeof(msg));
+			read(sockfd, msg, MAXLINE);
+			printf("%s", msg);
+			bzero(msg, sizeof(msg));
+		}
 	}
 	return 0;
+}
+
+void *recf( void * arg) {
+	sendinfo info = *((sendinfo*) arg);
+	free(arg);
+	char buf[MAXLINE];
+	sprintf(buf, "%d %d", info.part, info.all);
+	write(info.connfd, buf, strlen(buf));
+	write(info.connfd, info.name, strlen(info.name));
+	// get this time will eat how many char
+	read(info.connfd, buf, MAXLINE);
+	int sss = atoi(buf);
+	char mmmmmax[MAXLINE*20];
+	read(info.connfd, mmmmmax, sizeof(mmmmmax));
+	pthread_mutex_lock(&fileLock);
+	bzero(buf, sizeof(buf));
+	read(info.connfd, buf, sizeof(buf));
+	char dd[80];
+	sprintf(dd, "%s/%s", user, info.name);
+	file = fopen(dd, "w");
+	fseek(file, atoi(buf), SEEK_SET);
+	fwrite(mmmmmax, 1, sss, file);
+	puts(mmmmmax);
+	fclose(file);
+	pthread_mutex_unlock(&fileLock);
+	bzero(buf, sizeof(buf));
+	puts("recf Done...");
+	return NULL;
+}
+
+void *doSomething(void *arg) {
+	int connfd = *((int*) arg);
+	free(arg);
+	char buf[MAXLINE];
+
+	while(1) {
+		bzero(buf, sizeof(buf));
+		read(connfd, buf, MAXLINE);
+		if(!strcmp(buf, "gets")) {
+			read(connfd, buf, MAXLINE);
+			char dd[80];
+			sprintf(dd, "%s/%s", user, buf);
+
+			pthread_mutex_lock(&fileLock);
+			file = fopen(dd, "r");
+			fseek(file, 0, SEEK_END); // seek to end of file
+			int size = ftell(file); // get current file pointer
+			fclose(file);
+			pthread_mutex_unlock(&fileLock);
+			sprintf(buf, "%d", size);
+			write(connfd, buf, strlen(buf));
+			close(connfd);
+			printf("Done gets\n");
+			return NULL;
+		}
+		if(!strcmp(buf, "getf")) {
+			bzero(buf, sizeof(buf));
+			read(connfd, buf, MAXLINE);
+			int all, part;
+			char fname[40];
+			sscanf(buf, "%d%d", &part, &all);
+			bzero(buf, sizeof(buf));
+			read(connfd, buf, MAXLINE);
+			sprintf(fname, "%s", buf);
+			pthread_mutex_lock(&fileLock);
+			char dd[80];
+			sprintf(dd, "%s/%s", user, fname);
+			file = fopen(dd, "r");
+			fseek(file, 0, SEEK_END); // seek to end of file
+			int size = ftell(file); // get current file pointer
+			int thispart = size/all;
+			sprintf(buf, "%d", thispart);
+			write(connfd, buf, strlen(buf));
+
+			char send[thispart];
+			fseek(file, thispart*part, SEEK_SET);
+			fread(send, 1, thispart, file);
+			write(connfd, send, strlen(send));
+			fclose(file);
+			pthread_mutex_unlock(&fileLock);
+			sprintf(buf, "%d", thispart*part);
+			write(connfd, buf, strlen(buf));
+			close(connfd);
+			printf("Done getf\n");
+			return NULL;
+		}
+	}
 }
 
 void help () {
@@ -121,7 +366,10 @@ void help () {
 	printf("listu: list online user's infos\n");
 	printf("listf: list online user's files\n");
 	printf("updatef: update local files\n");
-	printf("connect: connect to other user\n");
+	printf("connect [ip] [port]: connect to other user\n");
+	printf("get [file]: get [file] from others\n");
+	//printf("put [file] [usr/ser]: put [file] to [usr/ser]\n");
+	printf("infos: get all users' infos\n");
 }
 
 void updatef(int sockfd) {
@@ -176,7 +424,8 @@ void* tcpListener() {
 	printf("tcpport: %d\n", port);
 	struct sockaddr_in tcpSer, client;
 	int tcpfd;
-
+	pthread_t tid;
+	int c = 0;
 	tcpfd = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&tcpSer, sizeof(tcpSer));
 
@@ -196,7 +445,9 @@ void* tcpListener() {
         connfd = accept(tcpfd, (SA *)&client, &chilen);
         printf("Connect...\nIP address is: %s, ", inet_ntoa(client.sin_addr));
         printf("port is: %d\n", (int) ntohs(client.sin_port));
-        write(connfd, "hi", strlen("hi"));
-        close(connfd);
+        int *confd;
+        confd = malloc(sizeof(int));
+        *confd = connfd;
+        pthread_create(&(tid), NULL, &doSomething, (void *)confd);
 	}
 }
--- HW3_102062335_Ser.c
+++ HW3_102062335_Ser.c
@@ -16,12 +16,17 @@ typedef struct {
 } Member;
 Member member[1024];
 pthread_mutex_t memberLock = PTHREAD_MUTEX_INITIALIZER;
-void init();
+char serfile[200][40];
+int serfileCount;
 
 void* service( void *argv);
+void listServerFile();
+FILE *file;
+pthread_mutex_t fileLock = PTHREAD_MUTEX_INITIALIZER;
 
 int main( int argc, char **argv) {
-	init();
+	
+	system("mkdir serData");
 
 	int sockfd;
 	pthread_t tid;
@@ -42,17 +47,10 @@ int main( int argc, char **argv) {
 		cliInfo = malloc(sizeof(clientSock));
 		socklen_t addrlen = sizeof(cliInfo->cliaddr);
 		cliInfo->clifd = accept(sockfd, (struct sockaddr*)&(cliInfo->cliaddr), &addrlen);
-		printf("%s\n", inet_ntoa(cliInfo->cliaddr.sin_addr));
 		pthread_create(&tid, NULL, &service, (void *)cliInfo);
 	}
 
 	return 0;
-}
-
-void init() {
-	/*- read some user data -*/
-	system("mkdir serData");
-	system("cd serData");
 }
 
 void* service( void *arg) {
@@ -72,7 +70,6 @@ void* service( void *arg) {
 			bzero(msg, sizeof(msg));
 			//get username
 			read(clisock.clifd, msg, MAXLINE);
-
 			pthread_mutex_lock(&memberLock);
 			int i, full = 1, idx;
 			for(i = 0; i < 1024; i++) {
@@ -92,7 +89,6 @@ void* service( void *arg) {
 				printf("welcome %s\n", member[idx].name);
 				printf("\t%s", inet_ntoa(member[i].socketaddr.sin_addr));
 				printf(" %d\n", member[i].port);
-				
 			}
 			//send final state
 			sprintf(msg, "%d", (full)?0:member[i].port);
@@ -177,12 +173,9 @@ void* service( void *arg) {
 			pthread_mutex_lock(&memberLock);
 			int i;
 			for(i = 0; i < 1024; i++) {
-				int kerker = 0;
 				if(member[i].valid == 1) {
 					char aaa[MAXLINE];
-					if(clisock.clifd == member[i].connfd) kerker = 1;
-					sprintf(aaa, "%s %s\n", member[i].name, 
-						(kerker)?"**": "");
+					sprintf(aaa, "%s %d\n", member[i].name, member[i].filecount);
 
 					strcat(msg, aaa);
 					int j;
@@ -192,17 +185,138 @@ void* service( void *arg) {
 							strcat(msg, aaa);
 						}
 					}
-					else {
-						sprintf(aaa, "no file\n");
-						strcat(msg, aaa);
-					}
 				}
 			}
 			pthread_mutex_unlock(&memberLock);
 			write(clisock.clifd, msg, strlen(msg));
 			bzero(msg, sizeof(msg));
-
 		}
+		if(!strcmp(msg, "infos")) {
+			puts("list all users' info");
+			bzero(msg, sizeof(msg));
+			strcpy(msg, "");
 
+			pthread_mutex_lock(&memberLock);
+			int i;
+			char aaa[MAXLINE];
+			for(i = 0; i < 1024; i++) {
+				if(member[i].valid == 1) {
+					
+					sprintf(aaa, "%s %d\n", member[i].name, member[i].filecount);
+					strcat(msg, aaa);
+					sprintf(aaa, "%s %d\n", inet_ntoa(member[i].socketaddr.sin_addr), member[i].port);
+					strcat(msg, aaa);
+					int j;
+					if(member[i].filecount) {
+						for(j = 0; j < member[i].filecount; j++) {
+							sprintf(aaa, "%s\n", member[i].files[j]);
+							strcat(msg, aaa);
+						}
+					}
+				}
+			}
+			pthread_mutex_unlock(&memberLock);
+			listServerFile();
+			sprintf(aaa, "%s %d\n", "server", serfileCount);
+			strcat(msg, aaa);
+			sprintf(aaa, "%s %d\n", "0.0.0.0", 0);
+			strcat(msg, aaa);
+			for(i = 0; i < serfileCount; i++) {
+				sprintf(aaa, "%s\n", serfile[i]);
+				strcat(msg, aaa);
+			}
+
+			write(clisock.clifd, msg, strlen(msg));
+			bzero(msg, sizeof(msg));
+		}
+		if(!strcmp(msg, "serf")) {
+			puts("list all files at server");
+			bzero(msg, sizeof(msg));
+			strcpy(msg, "");
+			strcat(msg, "server:\n");
+			
+			listServerFile();
+			
+			int i;
+			for(i = 0; i < serfileCount; i++) {
+				strcat(msg, serfile[i]);
+				strcat(msg, "\n");
+			}
+			
+			if(serfileCount == 0) {
+				strcat(msg, "no file\n");
+			}
+			
+			write(clisock.clifd ,msg, strlen(msg));
+			bzero(msg, sizeof(msg));
+		}
+		if(!strcmp(msg, "getf")) {
+			char buf[MAXLINE];
+			bzero(buf, sizeof(buf));
+			read(clisock.clifd, buf, MAXLINE);
+			int all, part;
+			char fname[40];
+			sscanf(buf, "%d%d", &part, &all);
+			bzero(buf, sizeof(buf));
+			read(clisock.clifd, buf, MAXLINE);
+			sprintf(fname, "%s", buf);
+			pthread_mutex_lock(&fileLock);
+			char dd[80];
+			sprintf(dd, "%s/%s", "serData", fname);
+			file = fopen(dd, "r");
+			fseek(file, 0, SEEK_END); // seek to end of file
+			int size = ftell(file); // get current file pointer
+			int thispart = size/all;
+			sprintf(buf, "%d", thispart);
+			write(clisock.clifd, buf, strlen(buf));
+
+			char send[thispart];
+			fseek(file, thispart*part, SEEK_SET);
+			fread(send, 1, thispart, file);
+			write(clisock.clifd, send, strlen(send));
+			fclose(file);
+			pthread_mutex_unlock(&fileLock);
+			sprintf(buf, "%d", thispart*part);
+			write(clisock.clifd, buf, strlen(buf));
+			close(clisock.clifd);
+			printf("Done getf\n");
+		}
+		if(!strcmp(msg, "gets")) {
+			read(clisock.clifd, buf, MAXLINE);
+			char dd[80];
+			sprintf(dd, "%s/%s", "serData", buf);
+			pthread_mutex_lock(&fileLock);
+			file = fopen(dd, "r");
+			fseek(file, 0, SEEK_END); // seek to end of file
+			int size = ftell(file); // get current file pointer
+			fclose(file);
+			pthread_mutex_unlock(&fileLock);
+			sprintf(buf, "%d", size);
+			write(clisock.clifd, buf, strlen(buf));
+			close(clisock.clifd);
+			printf("Done gets\n");
+		}
 	}
+}
+
+
+void listServerFile() {
+	pthread_mutex_lock(&fileLock);
+	system("ls -1 serData/ | wc -l >serlist");
+	file = fopen("serlist", "r");
+	
+	if(file != NULL) {
+		fscanf(file, "%d", &serfileCount);
+		fclose(file);
+	}
+	system("ls serData/ >serlist");
+	file = fopen("serlist", "r");
+	if(file != NULL) {
+		int i;
+		for(i = 0; i < serfileCount; i++) {
+			fscanf(file, "%s", serfile[i]);
+		}
+		fclose(file);
+	}
+	pthread_mutex_unlock(&fileLock);
 }
